fc0f20f5f6bf6c62c43f4d209c9c692a4ca1af8e
Lennin666 <soporte@allnexus.com.co>
2020-06-18 18:51:01 -0500

    Agregando y eliminando de favoritos

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -18,7 +18,14 @@ const Home = ({ myList, trends, originals }) => {
       {myList.length > 0 && (
         <Categories title='Mi Lista'>
           <Carousel>
-            {myList.map(item => <CarouselItem key={item.id} {...item} />)}
+            {myList.map(item => (
+              <CarouselItem
+                key={item.id}
+                {...item}
+                isList
+              />
+            ))}
+
           </Carousel>
         </Categories>
       )}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,19 @@
 const reducer = (state, action) => {
-  return state;
+
+  switch (action.type) {
+    case 'SET_FAVORITE':
+      return {
+        ...state,
+        myList: [...state.myList, action.payload],
+      };
+    case 'DELETE_FAVORITE':
+      return {
+        ...state,
+        myList: state.myList.filter(items => items.id !== action.payload),
+      };
+    default:
+      return state;
+  }
 };
 
 export default reducer;
